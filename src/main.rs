b08a7669711f70dd6cf6e15d5227b3777e9fd1f1
DarkBrave <darkbrave.darkbrave@gmail.com>
2026-06-22 15:55:16 -0500

    added audio back

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,10 @@ impl Dvd {
             hue: 0.0
         }
     }
-    fn update(&mut self, screen: &Vector2, dt: &f32) {
-        let mut update_hue = || {
+    fn update(&mut self, screen: &Vector2, dt: &f32, bounce_sound: &Sound) {
+        let mut bounce_events = || {
             self.hue = (self.hue + 30.0) % 360.0;
+            bounce_sound.play();
         };
 
         self.position.x += self.velocity.x * dt;
@@ -26,11 +27,11 @@ impl Dvd {
 
         if self.position.x <= 0.0 || self.position.x >= screen.x - self.size.x {
             self.velocity.x = -self.velocity.x;
-            update_hue();
+            bounce_events();
         }
         if self.position.y <= 0.0 || self.position.y >= screen.y - self.size.y {
             self.velocity.y = -self.velocity.y;
-            update_hue();
+            bounce_events();
         }
         self.position.x = self.position.x.clamp(0.0, screen.x - self.size.x);
         self.position.y = self.position.y.clamp(0.0, screen.y - self.size.y);
@@ -50,7 +51,7 @@ fn main() {
         .resizable()
         .build();
 
-    let dvd_texture: Texture2D = rl.load_texture(&thread, "dvd.png").unwrap();
+    let dvd_texture: Texture2D = rl.load_texture(&thread, "assets/dvd.png").unwrap();
     let dvd_source: Rectangle = Rectangle::new(0.0, 0.0, dvd_texture.width as f32, dvd_texture.height as f32);
 
     let mut dvd = Dvd::new(
@@ -59,11 +60,19 @@ fn main() {
         Vector2::new(150.0, 80.0)
     );
 
+    let audio: RaylibAudio = RaylibAudio::init_audio_device().unwrap();
+    let music: Music = audio.new_music("assets/music.ogg").unwrap();
+    let bounce_sound: Sound = audio.new_sound("assets/bounce.ogg").unwrap();
+    bounce_sound.set_volume(0.1);
+
+    music.play_stream();
+
     while !rl.window_should_close() {
+        music.update_stream();
         let dt: f32 = rl.get_frame_time();
         let screen: Vector2 = {Vector2::new(rl.get_screen_width() as f32, rl.get_screen_height() as f32)};
 
-        dvd.update(&screen, &dt);
+        dvd.update(&screen, &dt, &bounce_sound);
 
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
