c9380e9717fb359f413e7a212c7a73d6fb63885e
DarkBrave <darkbrave.darkbrave@gmail.com>
2026-06-22 14:00:19 -0500

    minor refactor to make code readable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::io;
 use raylib::prelude::*;
 
 fn main() {
@@ -6,9 +5,10 @@ fn main() {
     let (mut rl, thread) = raylib::init()
         .size(900, 600)
         .title("DVD")
+        .resizable()
         .build();
 
-    let mut current_hue: f32 = 0f32;
+    let mut current_hue: i32 = 0_i32;
 
     let texture: Texture2D = rl.load_texture(&thread, "dvd.png").unwrap();
     let dvd_source: Rectangle = Rectangle::new(0.0, 0.0, texture.width as f32, texture.height as f32);
@@ -22,35 +22,40 @@ fn main() {
 
     while !rl.window_should_close() {
         let dt = rl.get_frame_time();
+        let mut update_hue = || {
+            current_hue = (current_hue + 30) % 360;
+        };
+        let screen_width: f32 = rl.get_screen_width() as f32;
+        let screen_height: f32 = rl.get_screen_height() as f32;
 
         x_pos += velocity_x * dt;
         y_pos += velocity_y * dt;
 
-        if (x_pos <= 0f32) {
+        if x_pos <= 0f32 {
             x_pos = 0f32;
             velocity_x = -velocity_x;
-            current_hue = (current_hue + 30.0) % 360.0;
+            update_hue();
         }
-        if (x_pos >= rl.get_screen_width() as f32 - x_size) {
-            x_pos = rl.get_screen_width() as f32 - x_size;
+        if x_pos >= screen_width - x_size {
+            x_pos = screen_width - x_size;
             velocity_x = -velocity_x;
-            current_hue = (current_hue + 30.0) % 360.0;
+            update_hue();
         }
-        if (y_pos <= 0f32) {
+        if y_pos <= 0f32 {
             y_pos = 0f32;
             velocity_y = -velocity_y;
-            current_hue = (current_hue + 30.0) % 360.0;
+            update_hue();
         }
-        if (y_pos >= rl.get_screen_height() as f32 - y_size) {
-            y_pos = rl.get_screen_height() as f32 - y_size;
+        if y_pos >= screen_height - y_size {
+            y_pos = screen_height - y_size;
             velocity_y = -velocity_y;
-            current_hue = (current_hue + 30.0) % 360.0;
+            update_hue();
         }
 
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
         let dvd_logo: Rectangle = Rectangle::new(x_pos, y_pos, x_size, y_size);
-        let current_color = Color::color_from_hsv(current_hue, 1.0, 1.0);
+        let current_color = Color::color_from_hsv(current_hue as f32, 1.0, 1.0);
         d.draw_texture_pro(&texture, dvd_source, dvd_logo, Vector2::new(0.0, 0.0), 0.0, current_color);
     }
 }
