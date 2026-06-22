c6e5804baf969f52a159027df92751cae2780aa9
DarkBrave <darkbrave.darkbrave@gmail.com>
2026-06-22 15:02:05 -0500

    struct/impl-ified the dvd object

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
 use raylib::prelude::*;
 
+struct Dvd {
+    position: Vector2,
+    velocity: Vector2,
+    size: Vector2,
+    hue: f32
+}
+
+impl Dvd {
+    fn new(position: Vector2, velocity: Vector2, size: Vector2) -> Self {
+        Self {
+            position,
+            velocity,
+            size,
+            hue: 0.0
+        }
+    }
+    fn update(&mut self, screen: &Vector2, dt: &f32) {
+        let mut update_hue = || {
+            self.hue = (self.hue + 30.0) % 360.0;
+        };
+
+        self.position.x += self.velocity.x * dt;
+        self.position.y += self.velocity.y * dt;
+
+        if self.position.x <= 0.0 || self.position.x >= screen.x - self.size.x {
+            self.velocity.x = -self.velocity.x;
+            update_hue();
+        }
+        if self.position.y <= 0.0 || self.position.y >= screen.y - self.size.y {
+            self.velocity.y = -self.velocity.y;
+            update_hue();
+        }
+        self.position.x = self.position.x.clamp(0.0, screen.x - self.size.x);
+        self.position.y = self.position.y.clamp(0.0, screen.y - self.size.y);
+    }
+    fn draw(&self, d: &mut RaylibDrawHandle, texture: &Texture2D, source: &Rectangle) {
+        let dvd_logo: Rectangle = Rectangle::new(self.position.x, self.position.y, self.size.x, self.size.y);
+        let current_color = Color::color_from_hsv(self.hue, 1.0, 1.0);
+        d.draw_texture_pro(&texture, *source, dvd_logo, Vector2::new(0.0, 0.0), 0.0, current_color);
+    }
+}
+
 fn main() {
     println!("Test App");
     let (mut rl, thread) = raylib::init()
@@ -8,44 +50,24 @@ fn main() {
         .resizable()
         .build();
 
-    let mut current_hue: f32 = 0.0;
+    let dvd_texture: Texture2D = rl.load_texture(&thread, "dvd.png").unwrap();
+    let dvd_source: Rectangle = Rectangle::new(0.0, 0.0, dvd_texture.width as f32, dvd_texture.height as f32);
 
-    let texture: Texture2D = rl.load_texture(&thread, "dvd.png").unwrap();
-    let dvd_source: Rectangle = Rectangle::new(0.0, 0.0, texture.width as f32, texture.height as f32);
-
-    let mut x_pos: f32 = 450.0;
-    let mut y_pos: f32 = 300.0;
-    let x_size: f32 = 150.0;
-    let y_size: f32 = 80.0;
-    let mut velocity_x: f32 = 600.0;
-    let mut velocity_y: f32 = 300.0;
+    let mut dvd = Dvd::new(
+        Vector2::new(rl.get_screen_width() as f32/2.0, rl.get_screen_height() as f32/2.0),
+        Vector2::new(600.0, 300.0),
+        Vector2::new(150.0, 80.0)
+    );
 
     while !rl.window_should_close() {
-        let dt = rl.get_frame_time();
-        let mut update_hue = || {
-            current_hue = (current_hue + 30.0) % 360.0;
-        };
-        let screen_width: f32 = rl.get_screen_width() as f32;
-        let screen_height: f32 = rl.get_screen_height() as f32;
+        let dt: f32 = rl.get_frame_time();
+        let screen: Vector2 = {Vector2::new(rl.get_screen_width() as f32, rl.get_screen_height() as f32)};
 
-        x_pos += velocity_x * dt;
-        y_pos += velocity_y * dt;
-
-        if x_pos <= 0.0 || x_pos >= screen_width - x_size {
-            velocity_x = -velocity_x;
-            update_hue();
-        }
-        if y_pos <= 0.0 || y_pos >= screen_height - y_size {
-            velocity_y = -velocity_y;
-            update_hue();
-        }
-        x_pos = x_pos.clamp(0.0, screen_width - x_size);
-        y_pos = y_pos.clamp(0.0, screen_height - y_size);
+        dvd.update(&screen, &dt);
 
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
-        let dvd_logo: Rectangle = Rectangle::new(x_pos, y_pos, x_size, y_size);
-        let current_color = Color::color_from_hsv(current_hue, 1.0, 1.0);
-        d.draw_texture_pro(&texture, dvd_source, dvd_logo, Vector2::new(0.0, 0.0), 0.0, current_color);
+
+        dvd.draw(&mut d, &dvd_texture, &dvd_source);
     }
 }
